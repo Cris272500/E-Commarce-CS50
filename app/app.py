5f24b4194c319c0a5a9a377b03b1453833b6e782
Cris272500 <cristopherquincer@gmail.com>
2023-11-30 02:46:15 -0600

    Nuevas funciones
    Log out/log in del usuario osea el comprador hechos

--- app/app.py
+++ app/app.py
@@ -1,9 +1,10 @@
-from flask import Flask, request, render_template, redirect, url_for
+from flask import Flask, request, render_template, redirect, url_for, session
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.exc import IntegrityError
+from flask_bcrypt import Bcrypt
 
 
-from models import usuario, db
+from models import usuario, db, vendedores
 
 import sqlite3
 
@@ -11,7 +12,8 @@ import sqlite3
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Proyecto.db'  # 'sqlite:///site.db' indica que se usará SQLite y el archivo de la base de datos será 'site.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # Desactivar el seguimiento de modificaciones
-
+app.config['SECRET_KEY'] = 'tu_clave_secreta'  # Cambia a una clave secreta segura
+bcrypt = Bcrypt(app)
 db.init_app(app)
 
 # Crear la base de datos y las tablas
@@ -22,9 +24,18 @@ with app.app_context():
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-    mensaje = request.args.get('mensaje', None)
-    estado = request.args.get('estado', None) 
-    return render_template("index.html", mensaje=mensaje, estado=estado)
+    # Obteniendo id y name de la sesion
+    id = session.get('user_id', None)
+    name = session.get('user_name', None)
+
+    # Verificamos si ha iniciado sesion
+    if id is None or name is None:
+        print("Hola")
+        return render_template("index.html")
+    else:
+        mensaje = request.args.get('mensaje', None)
+        estado = request.args.get('estado', None) 
+        return render_template("index.html", mensaje=mensaje, estado=estado, name=name)
 
 @app.route("/comprador", methods=["GET", "POST"])
 def comprador():
@@ -47,10 +58,12 @@ def comprador():
         password = request.form.get("contra")
         direccion = request.form.get("direcc")
         
+        # Hasheamos la password
+        password_hash = bcrypt.generate_password_hash(password).decode('utf-8')
         
         try:
         # Crear un nuevo usuario y agregarlo a la base de datos
-            nuevo_usuario = usuario(nombre=name, email=correo, password_hash=password, direccion=direccion)
+            nuevo_usuario = usuario(nombre=name, email=correo, password_hash=password_hash, direccion=direccion)
             db.session.add(nuevo_usuario)
             db.session.commit()
             mensaje = "Usuario registrado exitosamente"
@@ -67,14 +80,84 @@ def comprador():
             db.session.close()
 
         return redirect(f'/comprador?mensaje={mensaje}&estado={estado}')
-        
+
+# Iniciar sesion comprador, usuario
+@app.route("/comprador_login", methods=["GET", "POST"])
+def comprador_login():
+    if request.method == "POST":
+        correo = request.form.get('email')
+        password = request.form.get('password')
+
+        usuario_existe = usuario.query.filter_by(email=correo).first()
+
+        # Validando que el usuario existe
+        if usuario_existe and bcrypt.check_password_hash(usuario_existe.password_hash, password):
+            mensaje = "Inicio de sesion exitosamente"
+            estado = "success_log"
+            session['user_id'] = usuario_existe.id
+            session['user_name'] = usuario_existe.nombre
+            return redirect(f'/?mensaje={mensaje}&estado={estado}')
+        else:
+            mensaje = "El usuario no existe"
+            estado = "error_log"
+            return redirect(f'/comprador_login?mensaje={mensaje}&estado={estado}')
+    else:
+        mensaje = request.args.get('mensaje', None)
+        estado = request.args.get('estado', None) 
+        return render_template("login.html", mensaje=mensaje, estado=estado)
 
 @app.route("/vendedor", methods=["GET", "POST"])
 def vendedor():
     if request.method == "GET":
-        return render_template("register.html")
+        mensaje = request.args.get('mensaje', None)
+        estado = request.args.get('estado', None)
+        return render_template("register.html", mensaje=mensaje, estado=estado)
     else:
-        pass
+        # obteniendo campos
+        nombre = request.form.get("nombre")
+        correo = request.form.get("correo")
+        password = request.form.get("contra")
+        telefono = int(request.form.get("numero"))
+        dir = request.form.get("direccion")
+
+        # Hasheamos la password
+        password_hash = bcrypt.generate_password_hash(password).decode('utf-8')
+
+        # validando que el correo no existe en la clase usuario
+        nuevo_vendedor = usuario.query.filter_by(email = correo).first()
+
+        if (nuevo_vendedor):
+           print("Waos")
+           mensaje = "El correo ya existe en vendedores"
+           estado = "error"
+           return redirect(f'/vendedor?mensaje={mensaje}&estado={estado}')
+        
+        try:
+            nuevo_vendedor = vendedores(nombre_vendedor=nombre, email_vendedor=correo, password_hash=password_hash, direccion_vendedor=dir, telefono=telefono)
+            db.session.add(nuevo_vendedor)
+            db.session.commit()
+            mensaje = "Vendedor registrado exitosamente"
+            estado = "success"
+        except IntegrityError:
+            db.session.rollback()
+            mensaje = "Hubo un error"
+            estado = "error"
+        except Exception:
+            db.session.rollback()
+            mensaje = "Hubo un error"
+            estado = "error"
+        finally:
+            db.session.close()
+        return redirect(f'/vendedor?mensaje={mensaje}&estado={estado}')
+
+#Cerrar sesion para el comprador
+@app.route('/comprador_logout')
+def comprador_logout():
+    session.pop('user_id', None)
+    session.pop('user_name', None)
+    
+    return redirect('/')
+
 # Main
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
--- app/models.py
+++ app/models.py
@@ -1,11 +1,63 @@
-from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy 
+from datetime import datetime
 
 db = SQLAlchemy()
 
 # Modelo de la base de datos
 class usuario(db.Model):
     id = db.Column(db.Integer, primary_key = True)
-    nombre = db.Column(db.String(20), nullable = False)
-    email = db.Column(db.String(120), nullable = False, unique = True)
-    password_hash = db.Column(db.String(15), nullable = False)
-    direccion = db.Column(db.String(120), nullable = False)
+    nombre = db.Column(db.String(50), nullable = True)
+    email = db.Column(db.String(120), nullable = True, unique = True)
+    password_hash = db.Column(db.String(50), nullable = True)
+    direccion = db.Column(db.String(120), nullable = True)
+
+class categoria_product(db.Model):
+    categoria_id = db.Column(db.Integer, primary_key = True)
+    name_category = db.Column(db.String(20),nullable = True)
+
+class productos(db.Model):
+    product_id = db.Column(db.Integer, primary_key = True)
+    nombre_producto = db.Column(db.String(20),nullable = True)
+    Descripcion = db.Column(db.String(500),nullable = False)
+    precio = db.Column(db.DECIMAL, nullable = True)
+    stock = db.Column(db.Integer,nullable = True)
+    categoria_id = db.Column(db.Integer,db.ForeignKey('categoria_product.categoria_id'), nullable = True)
+    
+class ordenes(db.Model):
+    orden_id = db.Column(db.Integer,primary_key = True)
+    cliente_id = db.Column(db.Integer, nullable = True)
+    fecha_orden = db.Column(db.DateTime, nullable = True)
+    estado = db.Column(db.String(10),nullable = True)
+    
+#class carrito(db.Model):
+    #carrito_id = db.Column(db.Integer, primary_key = True)
+    #cliente_id = db.Column(db.String(20),db.ForeignKey('usuario.cliente_id'), nullable = True)
+    #product_id = db.Column(db.Integer,db.ForeignKey('productos.product'), nullable = True)
+    
+class pago(db.Model):
+    pago_id = db.Column(db.Integer, primary_key = True, )
+    metodo_pago = db.Column(db.String(25), nullable = True)
+    pago_producto = db.Column(db.DECIMAL(25),nullable = True)
+    
+#class tarjetas(db.Model):
+    #numero_tarjeta = db.Column(db.String(19), db.ForeignKey('pago.metodo_pago'), nullable = True, unique = True)
+    #nombre_usuario = db.Column(db.String(50), db.ForeignKey('usuario.nombre_usuario'), nullable = True)
+    #cvc = db.Column(db.Integer, nullable = True, unique = True)
+    #correo = db.Column(db.String(50), db.ForeignKey('usuario.email'), nullable = True, unique = True)
+    
+#class recibo(db.Model):
+    #id = db.Column(db.Integer, primary_key = True, unique = True)
+    #usuario = db.Column(db.String(40), db.ForeignKey('usuario.id'), nullable = True, unique = True)
+    #product_id = db.Column(db.String(20), db.ForeignKey('producto_id.carrito'), nullable = True)
+    #fecha = db.Column(db.DateTime, nullable = True)
+    #total = db.Column(db.DECIMAL, nullable = True)
+    #pago_producto = db.Column(db.DECIMAL, db.ForeignKey('pago_product.pago'), nullable = True)
+    
+class vendedores(db.Model):
+    id_vendedor = db.Column(db.Integer, primary_key = True)
+    nombre_vendedor = db.Column(db.String(50), nullable = True)
+    email_vendedor = db.Column(db.String(120), nullable = True, unique = True)
+    password_hash = db.Column(db.String(15), nullable = True)
+    direccion_vendedor = db.Column(db.String(120), nullable = True)    
+    telefono = db.Column(db.Integer, nullable = True, unique = True)
+    
